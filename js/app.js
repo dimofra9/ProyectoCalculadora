7d1f0ee11bfbc173ce9fcd1700dfed057ed75bcd
Diego Morales <dimofra9@gmail.com>
2018-01-04 12:23:34 -0500

    paso 3
    se crean los métodos paara hacer que las teclas tengan efecto de presionado

--- js/app.js
+++ js/app.js
@@ -3,9 +3,18 @@ var Calculadora = {
 	ultOp: "",
 	cadena: "",
 //Metodo de Inicialización
-	init: function() {
+	init: function(){
 		this.asignarEventoBtn('teclado')
 	},
 	
-// 
+// Metodos al presionar Boton
+	asiganarEventoBtn: function(selector){
+		var teclas = document.querySelectorAll('.' + selector + ' img')
+		for(i=0;i<teclas.length;i++){
+			teclas[i].onclick = this.eventoPresionTecla
+			teclas[i].onmouseleave = this.eventoSoltoTecla
+		}
+	},
+	
+
 }
